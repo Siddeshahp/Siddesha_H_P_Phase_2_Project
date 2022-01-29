7512564ceff977a4363996249938638d0423880a
Siddeshahp <siddeshsid99@gmail.com>
2022-01-29 20:38:09 +0530

    commited

---
+++ Phase_2_Project/src/main/java/com/siddesh/AdminServlet.java
@@ -0,0 +1,75 @@
+package com.siddesh;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+@WebServlet("/adminServlet")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public AdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String oldPassword = request.getParameter("oldPass");
+		String newPassword = request.getParameter("newPass");
+		String confirmPassword = request.getParameter("confirmPass");
+		
+		if(newPassword.equals(confirmPassword))
+		{
+			admin admin = new admin();
+			admin.setOldPassword(oldPassword);
+			admin.setNewPassword(newPassword);
+			
+			DBConnection db = new DBConnection();
+			Connection conn = db.getConnection();
+
+			try {
+				
+					PreparedStatement ps = conn.prepareStatement(" Update login set password = ? where username = ?");
+					User user = (User) request.getSession().getAttribute("user");
+					ps.setString(2, user.getUsername());
+					ps.setString(1, newPassword);
+					ps.executeUpdate();
+					
+					response.sendRedirect("index.jsp");
+				
+				
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			} 
+			finally {
+				db.closeConnection();
+			}
+		}
+		else
+		{
+			
+		}
+	}
+}
---
+++ Phase_2_Project/src/main/java/com/siddesh/Booking.java
@@ -0,0 +1,56 @@
+package com.siddesh;
+
+public class Booking
+{
+	private String username;
+	private String fullName;
+	private int Age;
+	private String contact;
+	private String flightNo;
+	private String travelDate;
+	private int passenger;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getFlightNo() {
+		return flightNo;
+	}
+	public void setFlightNo(String flightNo) {
+		this.flightNo = flightNo;
+	}
+	public String getTravelDate() {
+		return travelDate;
+	}
+	public void setTravelDate(String travelDate) {
+		this.travelDate = travelDate;
+	}
+	public int getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(int passenger) {
+		this.passenger = passenger;
+	}
+	
+
+}
